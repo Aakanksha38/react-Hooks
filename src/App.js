81ae456db66526f0dca9200931eeb30434318591
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-03-10 17:20:38 +0530

    first commit from chapter-3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ErrorExample from './tutorial/1-useState/setup/1-error-example';
+function App() {
+  return (
+    <div className='container'>
+      <h2>
+        <ErrorExample/>
+      </h2>
+    </div>
+  )
+}
+
+export default App
+// export statment =  we want to use app component in some other folder , that's why we are exporting it .
