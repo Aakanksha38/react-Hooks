57173647b085d100cb89fd3ef2fb4a9b7e7b9cbe
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-03-11 09:20:28 +0530

    useStateObject example

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 //import ErrorExample from './tutorial/1-useState/setup/1-error-example';
 //import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
 //import UseStateArray from './tutorial/1-useState/final/3-useState-array';
-import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
-//import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
+//import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
 
 function App() {
   return (
     <div className='container'>
       <h2>
-        <UseStateArray/>
+        <UseStateObject/>
       </h2>
     </div>
   )
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -9,12 +9,14 @@ const UseStateObject = () => {
     exp: "2.8 years"
   });
   const handleClick =()=>{
-    setPerson({...person , job: "creative monk"})
+    // using spread operator of js , we copied whole object and changed one property of it . we can any no.of properties by mentioning it 
+    setPerson({...person , job: "creative monk", exp:"3 yrs"})
   }
   return (
         <div key={person.id}>
           <h3>{person.name}</h3>
           <h3>{person.job}</h3>
+          <h3>{person.exp}</h3>
           <button className='btn' onClick={handleClick}>Change text</button>
         </div>
       
