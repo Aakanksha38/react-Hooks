5314e9023505912859ceb34f15df5a472a0a9463
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-03-11 09:38:44 +0530

    increase,decrease,rest counter example

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 //import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
 //import UseStateArray from './tutorial/1-useState/final/3-useState-array';
 //import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
-import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
-
+//import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
+import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter';
 function App() {
   return (
     <div className='container'>
       <h2>
-        <UseStateObject/>
+        <UseStateCounter/>
       </h2>
     </div>
   )
