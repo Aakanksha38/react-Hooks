dd55cdd49d330349cb99b6d41420fb166b887a07
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-11-10 20:06:41 +0530

    prop-types

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({name,image,price}) => {
+  const imageUrl = image && image.url;
+
+  return(
+    <article className='product'>
+      <img src={imageUrl || defaultImage} alt={name || "default Image"} />
+      <h4>{name}</h4>
+      <h4>{price || 10.3}</h4>
+    </article>
+  );
 };
 
+
+
+
 export default Product;
+//whatever props we are using , we are specifying their type 
+//we use "isRequired" to make sure that a warning message is shown if the prop isn’t provided or if its data type is different from the specified one.
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+/*
+Product.defaultProps = {
+  name: 'default name', //Failed prop type: Invalid prop `price` of type `string` supplied to `Product`, expected `number`.
+  price: "3.99",
+  image: defaultImage,  //for image we specified proptype as object, here we r giving "string"...so warning we get in console:Failed prop type: Invalid prop `image` of type `string` supplied to `Product`, expected `object`.
+};
+*/
--- src/tutorial/10-prop-types/setup/index.js
+++ src/tutorial/10-prop-types/setup/index.js
@@ -4,10 +4,10 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-prop-types-example'
+const url = 'https://www.course-api.com/react-prop-types-example';
 
 const Index = () => {
-  const { products } = useFetch(url)
+  const { products } = useFetch(url); // this is custom hook , which requires "url" n it returns products n loading , n we are taking products
   return (
     <div>
       <h2>products</h2>
